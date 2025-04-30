d9d70cd3ef3170e7cff105493ee6847d9bbb5c1a
IsmaelEnrique <ismaeljulianenrique@gmail.com>
2025-04-30 16:57:24 -0300

    Resuelto conflicto

---
+++ Perfil.js
@@ -0,0 +1,4 @@
+function toggleInfo() {
+    const info = document.getElementById("info");
+    info.style.display = (info.style.display === "none" || info.style.display === "") ? "block" : "none";
+  }
